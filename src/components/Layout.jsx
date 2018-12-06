66c38441cf8eff93a834f9534b689cc2cdfe78fb
Cayce House <cayce@houseco.cc>
2018-12-06 15:17:45 -0500

    Added Prop Types

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Layout = ({ children }) => <div>{{ children }}</div>;
+
+Layout.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 
 export default Layout;
