1fe2ddd37e07ca1b4e91c8f0e80fc4fcdb04e9e5
Cayce House <cayce@houseco.cc>
2018-12-06 16:42:36 -0500

    Changed index typography

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 import Layout from '../components/Layout';
 
-export default () => <Layout>Hello world!</Layout>;
+export default () => (
+  <Layout>
+    <Typography variant="h1" gutterBottom align="center">
+      Welcome to the Index page.
+    </Typography>
+  </Layout>
+);
